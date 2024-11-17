8a18382bd8ca160720b699f85cc723ce834c9669
deepanshu <deepanshu120100@gmail.com>
2024-11-16 18:39:08 -0700

    Player Look Added
    +added freelook cinemachine
    +cinemachine in sync with player movement

--- PracticeGame/Assets/Scripts/PlayerController.cs
+++ PracticeGame/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
- 
+    public Transform cameraTransform;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 inputDirection = new Vector3(horizontal, 0, vertical).normalized;
+        
+        if (inputDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
-        transform.Translate(movement * (moveSpeed * Time.deltaTime), Space.World);
+            transform.Translate(moveDirection.normalized * (moveSpeed * Time.deltaTime), Space.World);
+        }
     }
 }
